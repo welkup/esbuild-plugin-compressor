745c0c64c481b13babc5efdd4f74a1b1ebdfbf49
Matt Soukup <matt@welkup.com>
2024-05-03 07:35:24 -0500

    preserve nested directory structure

--- index.mjs
+++ index.mjs
@@ -1,7 +1,32 @@
 import { compressContent } from './utils/compress-content.mjs';
 import { readFile, unlink } from 'fs/promises';
 import { outputFile } from 'fs-extra';
-import { join } from 'node:path';
+import { join, basename, dirname } from 'node:path';
+
+/**
+ * Gets build output directory based on the esbuild configuration.
+ * @type {import('./index.d.ts').getBuildOutputDir}
+ */
+function getBuildOutputDir({ outfile, outdir}) {
+  if (outdir) {
+    if (outdir.startsWith('./')) {
+      return outdir.substring(2);
+    }
+    return outdir;
+  }
+  if (outfile) {
+    const dir = dirname(outfile);
+    if (dir === '.') {
+      return '';
+    }
+    if (dir.startsWith('./')) {
+      return dir.substring(2);
+    }
+    return dir;
+  }
+  return '';
+//  throw new Error('Either outfile or outdir must be specified in the esbuild configuration');
+}
 
 /**
  * Esbuild plugin to compress files after build. Supports gzip and brotli compression.
@@ -13,22 +38,32 @@ export const compressor = options => ({
     build.initialOptions.metafile = true;
     build.onEnd(async result => {
       const outputs = result.metafile.outputs;
+
+      const buildOutputDir = getBuildOutputDir(build.initialOptions);
+      if (buildOutputDir) {
+        const firstOutput = Object.keys(outputs)[0];
+        if (firstOutput && !firstOutput.startsWith(buildOutputDir)) {
+          throw new Error(`Unexpected build output dir. Expected ${firstOutput} to begin with ${buildOutputDir}.`);
+        }
+      }
+
       const outputExt =
         options?.compressType?.toLowerCase() === 'brotli' ? '.br' : '.gz';
-      const outdir = options?.outdir || build.initialOptions.outdir;
+      const outdir = options?.outdir || buildOutputDir;
 
       //process files
       for (const filePath in outputs) {
         const fileType = filePath.substring(filePath.lastIndexOf('.') + 1);
 
         if (options?.fileTypes?.includes(fileType)) {
-          const fileName = filePath.split('/').at(-1);
+          const fileName = basename(filePath);
+          const nestedDirs = dirname(filePath).substring(buildOutputDir.length);
           const content = await readFile(filePath, 'utf8');
           const compressedContent = await compressContent(content, outputExt);
           const compressedFileName = `${fileName}${outputExt}`;
-          const compressedFilePath = join(outdir, compressedFileName);
+          const compressedFilePath = join(outdir, nestedDirs, compressedFileName);
 
-          // Save the gzipped file to the directory
+          // Save the compressed file to the directory
           await outputFile(compressedFilePath, compressedContent);
 
           // delete the original file
